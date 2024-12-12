17e0dab53ebb97773e54d8e8ef7eacf8fed4e2b8
RafiKastner <rafikastner@gmail.com>
2024-12-12 17:01:02 -0500

    moved app logic to separate file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import { useEffect, useState } from 'react';
 import './styles/App.css';
 import Sidebar from './components/sidebar';
 import Footer from './components/footer';
 import AddButton from './components/adder';
 import Main from './components/main';
+import { useAppLogic } from './scripts/appLogic';
 
 function App() {
-  var [dataTheme, setDataTheme] = useState(() => {
-    const storedTheme = localStorage.getItem("dataTheme")
-    if (storedTheme === null || storedTheme === undefined) {
-      localStorage.setItem("dataTheme", "light")
-      return "light"
-    }
-    return storedTheme
-  })
-
-  useEffect(() => {
-    document.documentElement.setAttribute('data-theme', dataTheme)
-    localStorage.setItem("dataTheme", dataTheme)
-  }, [dataTheme])
-
-  var color = dataTheme === 'light' ? 'black' : 'white'
-  var [notes, setNotes] = useState([0, 1, 2, 3, 4])
-  var [sidebarOpen, setSidebarOpen] = useState(true)
-  var props = { dataTheme, setDataTheme, color, notes, setNotes, sidebarOpen, setSidebarOpen}
+  const props = useAppLogic()
 
   return (
     <>
---
+++ src/scripts/appLogic.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+
+export function useAppLogic() {
+  const [dataTheme, setDataTheme] = useState(() => {
+    const storedTheme = localStorage.getItem("dataTheme");
+    if (!storedTheme) {
+      localStorage.setItem("dataTheme", "light");
+      return "light";
+    }
+    return storedTheme;
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', dataTheme);
+    localStorage.setItem("dataTheme", dataTheme);
+  }, [dataTheme]);
+
+  const color = dataTheme === 'light' ? 'black' : 'white';
+
+  const [notes, setNotes] = useState(() => {
+    let initialNotes = [];
+    for (let i = 0; i < 4; i++) {
+      initialNotes.push({ title: `Untitled ${i}`, id: i });
+    }
+    return initialNotes;
+  });
+
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  return {
+    dataTheme,
+    setDataTheme,
+    color,
+    notes,
+    setNotes,
+    sidebarOpen,
+    setSidebarOpen,
+  };
+}
