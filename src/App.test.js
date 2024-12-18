90ab1fe7e649e309ec2edf005f1eb7131f5c711d
RafiKastner <rafikastner@gmail.com>
2024-12-18 18:27:49 -0500

    Added test for sidebar

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { loremIpsum } from 'lorem-ipsum'
+import { randomIntFromInterval } from './scripts/generics';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Checks that sidebar cards render text correctly', () => {
+	var mockData = []
+	const lorem = []
+	for (let i = 0; i < 20; i++) {
+		lorem.push(loremIpsum({
+			count: randomIntFromInterval(1, 10),
+			units: 'words',
+			format: 'plain'
+		}))
+	}
+	for (let i = 0; i < lorem.length; i+=2) {
+		mockData.push({
+			id: i / 2,
+			title: lorem[i],
+			description: lorem[i+1],
+		})
+	}
+	render(<App data={mockData}/>);
+	for (let i = 0; i < lorem.length; i++) {
+		const linkElement = screen.getByText(lorem[i]);
+		expect(linkElement).toBeInTheDocument();
+	}
 });
